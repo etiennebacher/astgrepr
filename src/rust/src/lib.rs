e2c199033267f45135e2c0c0f41d3383baebb1ab
etiennebacher <etienne.bacher@protonmail.com>
2024-03-03 10:14:17 +0100

    init

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -5,19 +5,25 @@ pub mod ser;
 use node::SgNode;
 // use range::{Pos, Range};
 
+use ast_grep_core::language::TSLanguage;
 use ast_grep_core::{AstGrep, Language, NodeMatch, StrDoc};
 use ast_grep_language::SupportLang;
 
 #[derive(Clone)]
 pub struct SgRoot {
-    inner: AstGrep<StrDoc<SupportLang>>,
+    inner: AstGrep<StrDoc<TSLanguage>>,
     filename: String,
 }
 
 #[extendr]
 impl SgRoot {
     fn new(src: &str, lang: &str) -> Self {
-        let lang = SupportLang::from(lang.parse().unwrap());
+        // let lang = SupportLang::from(lang.parse().unwrap());
+
+        // let mut parser = tree_sitter::Parser::new();
+        let rlang = tree_sitter_r::language();
+        let lang = TSLanguage::from(rlang);
+
         let inner = lang.ast_grep(src);
         Self {
             inner,
