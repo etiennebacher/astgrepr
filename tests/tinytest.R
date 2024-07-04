47bfde3dff72b812bd9096d110d4ed74d90083a5
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-07-04 17:01:45 +0200

    fix spelling file

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -2,7 +2,3 @@
 if (requireNamespace("tinytest", quietly=TRUE)){
   tinytest::test_package("astgrepr", testdir = "tinytest")
 }
-
-if(requireNamespace('spelling', quietly = TRUE)) {
-  spelling::spell_check_test(vignettes = TRUE, error = FALSE, skip_on_cran = TRUE)
-}
