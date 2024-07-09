2a64fea84a08c13e9e3c9bd0d3e0f3fcc374c87e
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-07-09 08:21:36 +0200

    fix metavar

--- R/node.R
+++ R/node.R
@@ -853,7 +853,7 @@ node_replace_all <- function(x, ...) {
     if (is.null(x[[y]])) return(invisible())
     id <- names(x)[y]
     repl <- replacements[[id]]
-    meta_var <- regmatches(repl, gregexpr("~~([A-Z]+)~~", repl))[[1]]
+    meta_var <- regmatches(repl, gregexpr("~~([A-Z0-9]+)~~", repl))[[1]]
     meta_var <- gsub("~~", "", meta_var)
 
     lapply(x[[y]], function(z) {
