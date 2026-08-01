d7cd708fa47e928311cbdb3bb66d0b1e4415833a
Jeroen Ooms <jeroenooms@gmail.com>
2026-08-01 15:23:32 +0200

    Fix build on Windows ARM64:  - Set the correct --target flag  - Update to extendr 0.8.2

--- tools/config.R
+++ tools/config.R
@@ -66,6 +66,18 @@ cfg <- if (is_debug) "debug" else "release"
 # read in the Makevars.in file checking
 is_windows <- .Platform[["OS.type"]] == "windows"
 
+if(is_windows){
+  .target <- if(grepl("aarch", R.version$platform)){
+    "aarch64-pc-windows-gnullvm"
+  } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
+    "x86_64-pc-windows-gnullvm"
+  } else if(grepl("i386", R.version$platform)){
+    "i686-pc-windows-gnu"
+  } else {
+    "x86_64-pc-windows-gnu"
+  }
+}
+
 # if windows we replace in the Makevars.win.in
 mv_fp <- ifelse(
   is_windows,
